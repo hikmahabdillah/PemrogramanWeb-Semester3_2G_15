17b9323cf92c253f1fa19f9c02bd54fd9bbee7f4
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-09-27 08:53:57 +0700

    Praktikum 1 | Soal No 4

--- Pertemuan 5/fungsi.php
+++ Pertemuan 5/fungsi.php
@@ -1,15 +1,9 @@
 <?php
 //membuat fungsi 
-function perkenalan($nama, $salam = "Assalamualaikum")
-{
-  echo $salam . ", ";
-  echo "Perkenalkan, nama saya " . $nama . "<br/>";
-  echo "Senang berkenalan dengan Anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang) { 
+  $umur = $thn_sekarang -  $thn_lahir;
+  return $umur;
 }
-//memanggil fungsi yang sudah dibuat 
-perkenalan("Hamdana", "Hallo");
-echo "<hr>";
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-//memanggil lagi 
-perkenalan($saya);
+echo "Umur saya adalah ". hitungUmur(2005, 2024) . "tahun";
+ // isi sesuai dengan tahun lahir kalian
+?>
