be1383e04bce49f9515c3c16d10b5eb00af5b50f
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-10-04 07:52:52 +0700

    Praktikum 1 | Soal No 1

---
+++ Pertemuan 6/array_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+  <?php
+    $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+
+    echo $Listdosen[2] . "<br>";
+    echo $Listdosen[0] . "<br>";
+    echo $Listdosen[1] . "<br>";
+  ?>
+</body>
+</html>
